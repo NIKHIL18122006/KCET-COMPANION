550ad709bc46ed8f5f792adb76c08d249ae087ec
NIKHIL <nikhi2866@gmail.com>
2026-08-17 17:29:18 +0530

    prepare for deploymentt

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://kcet-companion.vercel.app",
     credentials: true,
 }));
 app.use(express.json());
