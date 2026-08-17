e0d9ae4488ed92d0adb20245f9720f20b07173a2
NIKHIL <nikhi2866@gmail.com>
2026-08-17 17:11:15 +0530

    prepare for frontend deployment

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://kcet-companion-g1kv.onrender.com/api",
     withCredentials: true,
 });
 
